37fd5cfd4b5a6924766d44c48df6dd729a640ea6
Michael Gharbi <gharbi@mit.edu>
2015-06-25 16:58:37 -0400

    deconv normalize

--- src/caffe/util/im2col.cu
+++ src/caffe/util/im2col.cu
@@ -78,8 +78,10 @@ __global__ void col2im_gpu_kernel(const int n, const Dtype* data_col,
     const int stride_h, const int stride_w,
     const int height_col, const int width_col,
     Dtype* data_im) {
+
   CUDA_KERNEL_LOOP(index, n) {
     Dtype val = 0;
+    int vcount = 0;
     int w = index % width + pad_w;
     int h = (index / width) % height + pad_h;
     int c = index / (width * height);
@@ -88,16 +90,7 @@ __global__ void col2im_gpu_kernel(const int n, const Dtype* data_col,
     int w_col_end = min(w / stride_w + 1, width_col);
     int h_col_start = (h < patch_h) ? 0 : (h - patch_h) / stride_h + 1;
     int h_col_end = min(h / stride_h + 1, height_col);
-    /*
-    for (int h_col = h_col_start; h_col < h_col_end; ++h_col) {
-      for (int w_col = w_col_start; w_col < w_col_end; ++w_col) {
-        // the col location: [c * width * height + h_out, w_out]
-        int c_col = c * patch_h * patch_w + (h - h_col * stride_h) * ksize
-            + (w - w_col * stride_w);
-        val += data_col[(c_col * height_col + h_col) * width_col + w_col];
-      }
-    }
-    */
+
     // equivalent implementation
     int offset =
         (c * patch_h * patch_w + h * patch_w + w) * height_col * width_col;
@@ -106,10 +99,20 @@ __global__ void col2im_gpu_kernel(const int n, const Dtype* data_col,
     for (int h_col = h_col_start; h_col < h_col_end; ++h_col) {
       for (int w_col = w_col_start; w_col < w_col_end; ++w_col) {
         val += data_col[offset + h_col * coeff_h_col + w_col * coeff_w_col];
+        vcount += 1;
       }
     }
     data_im[index] = val;
+    if(vcount > 0) {
+        data_im[index] /= vcount;
+    }
+    // count[index] = vcount;
   }
+  // CUDA_KERNEL_LOOP(index, n) {
+  //       if(count[index] > 0) {
+  //           data_im[index] /= count[index];
+  //       }
+  // }
 }
 
 template <typename Dtype>
