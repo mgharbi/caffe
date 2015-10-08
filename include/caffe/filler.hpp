6c12e4c01b7bb3faee714fb7e08c027932703f1b
Michael Gharbi <gharbi@mit.edu>
2015-10-08 13:25:26 -0400

    nofill

--- include/caffe/filler.hpp
+++ include/caffe/filler.hpp
@@ -262,29 +262,6 @@ class BilinearFiller : public Filler<Dtype> {
   }
 };
 
-// ------------------ MGH ------------------------------------------
-template <typename Dtype>
-class DerivativeFiller : public Filler<Dtype> {
- public:
-  explicit BilinearFiller(const FillerParameter& param)
-      : Filler<Dtype>(param) {}
-  virtual void Fill(Blob<Dtype>* blob) {
-    CHECK_EQ(blob->num_axes(), 4) << "Blob must be 4 dim.";
-    CHECK_EQ(blob->shape(0), blob->shape(1)*2) << "Derivative must have twice as many outputs.";
-    CHECK_EQ(blob->width(), blob->height()) << "Filter must be square";
-    Dtype* data = blob->mutable_cpu_data();
-
-    int f = ceil(blob->width() / 2.);
-    float c = (2 * f - 1 - f % 2) / (2. * f);
-    for (int i = 0; i < blob->count(); ++i) {
-      float x = i % blob->width();
-      float y = (i / blob->width()) % blob->height();
-      data[i] = (1 - fabs(x / f - c)) * (1 - fabs(y / f - c));
-    }
-    CHECK_EQ(this->filler_param_.sparse(), -1)
-         << "Sparsity not supported by this Filler.";
-  }
-};
 
 /**
  * @brief Get a specific filler from the specification given in FillerParameter.
