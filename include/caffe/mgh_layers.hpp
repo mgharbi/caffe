7fd75313daf11ae3b81d36ef05b5d5b39a74cd6c
Michael Gharbi <gharbi@mit.edu>
2015-12-24 07:09:37 -0500

    new channels

--- include/caffe/mgh_layers.hpp
+++ include/caffe/mgh_layers.hpp
@@ -75,6 +75,113 @@ class OffsetCropLayer : public Layer<Dtype> {
         int offset_y;
 };
 
+template <typename Dtype>
+class MosaicLayer : public Layer<Dtype> {
+ public:
+  explicit MosaicLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "Mosaic"; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+      NOT_IMPLEMENTED;
+  }
+};
+
+template <typename Dtype>
+class KillChannelLayer : public Layer<Dtype> {
+ public:
+  explicit KillChannelLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "KillChannel"; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+      NOT_IMPLEMENTED;
+  }
+
+  int Rand(int n);
+
+  shared_ptr<Caffe::RNG> rng_;
+};
+
+template <typename Dtype>
+class PermuteChannelsLayer : public Layer<Dtype> {
+ public:
+  explicit PermuteChannelsLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "PermuteChannels"; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+      NOT_IMPLEMENTED;
+  }
+  int Rand(int n);
+
+  shared_ptr<Caffe::RNG> rng_;
+};
+
+template <typename Dtype>
+class RGB2XYZLayer : public Layer<Dtype> {
+ public:
+  explicit RGB2XYZLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "RGB2XYZ"; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //     const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  // RGB2XYZParameter_RGB2XYZOp op_;
+  // vector<Dtype> coeffs_;
+  // Blob<int> max_idx_;
+
+  // bool stable_prod_grad_;
+};
+
 }  // namespace caffe
 
 #endif /* end of include guard: MGH_LAYERS_HPP_WXBM6UCI */
