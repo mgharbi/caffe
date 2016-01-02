b4856354207decd989eea314271f1def42f9c816
Michael Gharbi <gharbi@mit.edu>
2016-01-02 17:35:08 -0500

    rgb2hsv

--- include/caffe/mgh_layers.hpp
+++ include/caffe/mgh_layers.hpp
@@ -152,6 +152,32 @@ class PermuteChannelsLayer : public Layer<Dtype> {
 };
 
 template <typename Dtype>
+class RGB2HSVLayer : public Layer<Dtype> {
+ public:
+  explicit RGB2HSVLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "RGB2HSV"; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  // virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+  //     const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+  // virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+  //     const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+};
+
+template <typename Dtype>
 class RGB2XYZLayer : public Layer<Dtype> {
  public:
   explicit RGB2XYZLayer(const LayerParameter& param)
---
+++ src/caffe/layers/rgb2hsv_layer.cpp
@@ -0,0 +1,116 @@
+#include <cfloat>
+#include <vector>
+#include <cmath>
+
+#include "caffe/layer.hpp"
+#include "caffe/util/math_functions.hpp"
+#include "caffe/mgh_layers.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void RGB2HSVLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  CHECK_EQ(bottom[0]->shape()[1], 3) << "Input to rgb2hsv layer should have 3 channels" ;
+  CHECK_NE(top[0], bottom[0]) << this->type() << " Layer does not "
+    "allow in-place computation.";
+}
+
+template <typename Dtype>
+void RGB2HSVLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  for (int i = 1; i < bottom.size(); ++i) {
+    CHECK(bottom[i]->shape() == bottom[0]->shape());
+  }
+  top[0]->ReshapeLike(*bottom[0]);
+}
+
+template <typename Dtype>
+void RGB2HSVLayer<Dtype>::Forward_cpu(
+    const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+    Dtype* top_data = top[0]->mutable_cpu_data();
+
+    vector<int> shape = bottom[0]->shape();
+    for (int n = 0; n < shape[0]; ++n) 
+    for (int y = 0; y < shape[2]; ++y) 
+    for (int x = 0; x < shape[3]; ++x) 
+    {
+        Dtype r = bottom_data[bottom[0]->offset(n,0,y,x)];
+        Dtype g = bottom_data[bottom[0]->offset(n,1,y,x)];
+        Dtype b = bottom_data[bottom[0]->offset(n,2,y,x)];
+
+        Dtype maxi = std::max(r,std::max(g,b));
+        Dtype mini = std::min(r,std::min(g,b));
+
+        Dtype delta = maxi-mini;
+
+        Dtype h = 0;
+        Dtype v = 0; 
+        Dtype s = 0; 
+
+        // Value
+        v = maxi;
+
+        // Saturation
+        if(v == 0 || delta == 0) {
+            s = 0;
+        }else{
+            s = delta/v;
+        }
+
+        // Hue
+        if(delta == 0){
+            h = 0;
+        }else {
+            if(maxi == r) {
+                h = (g-b)/delta;
+            }
+            if(maxi == g) {
+                h = 2.0 + (b-r)/delta;
+            }
+            if(maxi == b) {
+                h = 4.0 + (r-g)/delta;
+            }
+            h = std::fmod((h/6.0),1.0);
+        }
+
+        top_data[top[0]->offset(n,0,y,x)] = h;
+        top_data[top[0]->offset(n,1,y,x)] = s;
+        top_data[top[0]->offset(n,2,y,x)] = v;
+    }
+}
+
+template <typename Dtype>
+void RGB2HSVLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
+  const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
+
+    const Dtype* top_diff = top[0]->cpu_diff();
+    const Dtype* bottom_data = bottom[0]->cpu_data();
+
+    Dtype* bottom_diff = bottom[0]->mutable_cpu_diff();
+
+    vector<int> shape = bottom[0]->shape();
+    for (int n = 0; n < shape[0]; ++n) 
+    for (int y = 0; y < shape[2]; ++y) 
+    for (int x = 0; x < shape[3]; ++x) 
+    {
+        Dtype dh = top_diff[top[0]->offset(n,0,y,x)];
+        Dtype ds = top_diff[top[0]->offset(n,1,y,x)];
+        Dtype dv = top_diff[top[0]->offset(n,2,y,x)];
+
+        bottom_diff[bottom[0]->offset(n,0,y,x)] = 0;
+        bottom_diff[bottom[0]->offset(n,1,y,x)] = 0;
+        bottom_diff[bottom[0]->offset(n,2,y,x)] = 0;
+    }
+}
+
+#ifdef CPU_ONLY
+STUB_GPU(RGB2HSVLayer);
+#endif
+
+INSTANTIATE_CLASS(RGB2HSVLayer);
+REGISTER_LAYER_CLASS(RGB2HSV);
+
+}  // namespace caffe
